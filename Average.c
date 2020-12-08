b65e2facb52e3358c4721a29ecbd9ac9efb2f666
Arnaud Maillard <arnaud.maillard@heig-vd.ch>
2020-12-08 16:58:37 +0100

    Programme done

--- Average.c
+++ Average.c
@@ -1,47 +1,68 @@
+/**
+ * Présentation de l'exercice...
+ * - Déclarer référentiel GitHub exercice (4min)
+ * - Déclarer un tableau de 128 float 32-bits (5min) -> 13h30
+ * - Remplir ce tableau avec des valeurs aléatoires 0..255 (10min)
+ * - Identifier la valeur max. du tableau et la valeur min. puis afficher (5min)
+ * - Normaliser ce tableau à 1.0 (10min)
+ * - Afficher le tableau sur la sortie standard (5min) 
+ **/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
-// void fill (float array[]){
 
-// }
-
-// void display_min_max (const float array[]){
-//     printf("Max = %f\n", );
-//     printf("Min = %f\n", );
-// }
-
-int main (){
-    // Initialise the array
-    float array[128] = {0};
-    const size_t size = sizeof(array) /  sizeof(array[0]);
-
-    // Fill-in the array
-    float max = FLT_MIN, min = FLT_MAX;
+// Fill-in the array
+void fill (float array[], size_t size){
     for (int i = 0; i < size; i++){
         array[i] = (float)rand()/RAND_MAX*255;
     }
+}
 
-    // Identify the max and min value
+// Identify the max value
+float max (const float array[], size_t size){
+    float max = FLT_MIN;
     for (int i = 0; i < size; i++){
         if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+// Identify the min value
+float min(const float array[], size_t size){
+    float min = FLT_MAX;
+    for (int i = 0; i < size; i++){
         if (array[i] < min) min = array[i];
     }
-    
-    // Normalize
+    return min;
+}
+
+// Noramlize the array with values between 0.0 and 1.0
+void normalize (float array[], size_t size){
+    float max_value = max(array, size);
     for(int i = 0; i < size; i++){
-        array[i] = (array[i]/max);
+        array[i] = array[i]/max_value;
+    }
+}
+
+// Display the values of the array
+void display (const float array[], size_t size){
+    for(int i = 0; i < size; i++){
         printf("%f\n", array[i]);
     }
+}
 
-    // Identify the max and min value
-    max = FLT_MIN, min = FLT_MAX;
-    for (int i = 0; i < size; i++){
-        if (array[i] > max) max = array[i];
-        if (array[i] < min) min = array[i];
-    }
+int main (){
+    // Initialize the array
+    float array[128] = {0};
+    const size_t size = sizeof(array) /  sizeof(array[0]);
 
+    fill(array, size);
+    normalize(array, size);
+    display(array, size);
+    
     // Display max and min value
-    printf("Max = %f\n", max);
-    printf("Min = %f\n", min);
+    printf("Max = %f\n", max(array, size));
+    printf("Min = %f\n", min(array, size));
     
 }
